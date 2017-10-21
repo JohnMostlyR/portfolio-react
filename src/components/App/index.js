b6000a4769b53a6f98c2ffc568624d05deeb2190
Johan Meester <walter.doodah@gmail.com>
2017-10-21 21:13:08 +0200

    Removing unused import FormattedMessage.

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -5,7 +5,7 @@ import {
   Redirect,
 } from 'react-router-dom';
 import Helmet from 'react-helmet';
-import {defineMessages, FormattedMessage, injectIntl, intlShape} from 'react-intl';
+import {defineMessages, injectIntl, intlShape} from 'react-intl';
 import styled from 'styled-components';
 
 import SiteHeader from '../SiteHeader/index';
